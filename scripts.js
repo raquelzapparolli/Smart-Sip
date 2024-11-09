7ad3a8a25bd6eb71c5e14e29f498d806d5fb8f80
Raquel Zapparolli <164427602+raquelzapparolli@users.noreply.github.com>
2024-11-08 23:01:15 -0300

    Subir os arquivos do site

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Script para interagir com o botão "Comprar Agora"
+document.getElementById('comprar-btn').addEventListener('click', function () {
+    alert('Obrigado por comprar o Smart Sip! O seu pedido está sendo processado.');
+});
